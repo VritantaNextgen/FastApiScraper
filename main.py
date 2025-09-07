70eee1d17d48d6ba724247c4152068413258126d
Amit Sharma <vritantanextgen@gmail.com>
2025-09-07 14:25:40 +0530

    production testing 2

--- main.py
+++ main.py
@@ -27,16 +27,7 @@ def get_product_details(product_name: str):
     try:
         # Selenium Chrome options
         chrome_options = Options()
-        chrome_options.add_argument("--headless=new")  # Headless mode
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                                    "AppleWebKit/537.36 (KHTML, like Gecko) "
-                                    "Chrome/115.0.0.0 Safari/537.36")
-        # ✅ Create a unique temporary profile directory
-        user_data_dir = tempfile.mkdtemp()
-        chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+        chrome_options.add_argument("--headless")  # Headless mode
 
 
         driver = webdriver.Chrome(options=chrome_options)
